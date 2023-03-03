9aab745e97558db5898324a8a96efe3241ee066d
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2023-03-03 14:15:35 +0000

    Lesson 01: Java and Kotlin HelloWorld

---
+++ lesson01/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public HelloWorld(int x) { }
+    public HelloWorld() { }
+
+    public static void main(String[] args) {
+        System.out.println("SLB in Java");
+    }
+}
